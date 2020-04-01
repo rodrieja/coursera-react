cc302d0e29218397a04c03ed21f6c6731dbfb95e
Alejandro Rodriguez <aerodriguez@baufest.com>
2020-04-01 19:58:40 -0300

    React Redux Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -42,9 +42,9 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
-                    dish={this.state.dishes.find((dish) => dish.featured)}
-                    promotion={this.state.promotions.find((promo) => promo.featured)}
-                    leader={this.state.leaders.find((leader) => leader.featured)}
+                    dish={this.props.dishes.find((dish) => dish.featured)}
+                    promotion={this.props.promotions.find((promo) => promo.featured)}
+                    leader={this.props.leaders.find((leader) => leader.featured)}
                 />
             );
         }
@@ -52,8 +52,8 @@ class Main extends Component {
         const DishWithId = ({ match }) => {
             return (
                 <DishDetail
-                    dish={this.state.dishes.find((dish) => dish.id === parseInt(match.params.dishId, 10))}
-                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    dish={this.props.dishes.find((dish) => dish.id === parseInt(match.params.dishId, 10))}
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
                 />
             );
         };
@@ -63,10 +63,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
